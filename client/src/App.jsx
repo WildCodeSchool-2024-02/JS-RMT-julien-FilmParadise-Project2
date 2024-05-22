75f7dd46f15d23c9ca557681bb0da8e6d4acd50c
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-05-22 16:07:57 +0200

    correction app.jsx pour PR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
-import { Outlet } from "react-router-dom";
-
 import Header from "./components/header/Header";
 import "./App.css";
 
-
 function App() {
   return (
-  <div>
-    <Header />
-    <main>
-      <Outlet/>
-    </main>
-  </div>
+    <div>
+      <header>
+        <Header />
+      </header>
+    </div>
   );
 }
 
