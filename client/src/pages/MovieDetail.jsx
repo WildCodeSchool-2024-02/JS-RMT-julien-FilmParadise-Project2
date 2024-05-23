24016f984a33e08c1a5c833025a0a6a9c75c4a2a
Johnny Domingues <johnnydomingues@sfr.fr>
2024-05-23 12:06:18 +0200

    initilaisation de la page MovieDetail

--- client/src/pages/MovieDetail.jsx
+++ client/src/pages/MovieDetail.jsx
@@ -1,13 +1,31 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function MovieDetail() {
-  
+  const { title } = useParams();
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3310/api/movies/${title}`)
+      .then((response) => {
+        setMovie(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the movie details!", error);
+      });
+  }, [title]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <section>
-      <h1>page détail d'un film</h1>
-    </section>
-  )
-};
+    <div>
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+    </div>
+  );
+}
 
 export default MovieDetail;
--- server/app/router.js
+++ server/app/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const movies = require("../database/data");
+
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
@@ -11,6 +13,17 @@ const router = express.Router();
 // Route to get a list of items
 router.get("/movies", (req, res) => {
   res.send("hello world");
+});
+
+router.get("/movies/:title", (req, res) => {
+  const wantedTitle = req.params.title;
+  const movie = movies.find((element) => element.title === wantedTitle);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(404).json({ message: "Movie not found" });
+  }
 });
 
 // Route to get a specific item by ID
