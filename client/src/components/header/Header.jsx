f293892ee41137c060bdc1519257a837416948ec
Vanessa <nessa_g@hotmail.fr>
2024-05-23 17:01:21 +0200

    Initialisation du header, Titre, phrase d'accroche, logo et bouton de navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,22 @@
-
+import './header.css';
+import logo from "../../assets/images/Logo.png"
 
 function Header() {
-  
-
   return (
-    <header>
-      <h1> mon header</h1>
+    <header className="header">
+      <div className="logo-section">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="title-section">
+          <h1 className="title">Film Paradise</h1>
+          <p className="subtitle">"Le ciné dans ton canapé"</p>
+        </div>
+      </div>
+      <div className="action-buttons">
+        <button type='button' className="favorites-button">Favoris</button>
+        <button type='button' className="cart-button">Panier</button>
+      </div>
     </header>
-  )
-};
+  );
+}
 
 export default Header;
