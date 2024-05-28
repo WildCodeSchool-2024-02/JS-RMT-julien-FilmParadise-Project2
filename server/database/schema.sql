102f393fca7df4500d3f50fb7df88269283d0753
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-05-28 16:16:00 +0200

    creation de la table et insertion des 8 premiers films

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,4 +1,11 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+create table movies (
+  id int primary key auto_increment not null,
+  title varchar(255) not null,
+  poster_path varchar(255) not null,
+  realease_date date not null,
+  vote_average int not null,
+  vote_count int not null,
+  price int not null,
+  genre_ids varchar(255) not null,
+  popularity int not null,
 );
