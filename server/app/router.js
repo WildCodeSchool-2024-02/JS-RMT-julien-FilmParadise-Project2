a033c9751f1855120dfc69c76e3ecde9e1bb4861
Johnny Domingues <johnnydomingues@sfr.fr>
2024-05-29 10:01:20 +0200

    gestion du conflit dans le router

--- server/app/router.js
+++ server/app/router.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const movies = require("../database/data");
-
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
