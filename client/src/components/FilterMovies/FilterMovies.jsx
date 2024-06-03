28b6bedf1d43d73b017ced3637d422454de2f75c
AnnDo33 <ann.dominique.d@gmail.com>
2024-06-03 16:45:26 +0200

    Modif categories

--- client/src/components/FilterMovies/FilterMovies.jsx
+++ client/src/components/FilterMovies/FilterMovies.jsx
@@ -4,15 +4,17 @@ function FilterMovies({ selectedGenre, handleGenreChange }) {
   // Dans le return mettre en place un label avec un select et option basé sur le tableau categories
   return (
     <div className="filter-movies">
-      <label htmlFor="genre">Select Genre: </label>
-      <select id="genre" value={selectedGenre} onChange={handleGenreChange}>
-        <option value="">All Genres</option>
-        {categories.map((categorie) => (
-          <option key={categorie} value={categories}>
-            {categories}
-          </option>
-        ))}
-      </select>
+      <label htmlFor="genre">
+        Select Genre:
+        <select id="genre" value={selectedGenre} onChange={handleGenreChange}>
+          <option value="">All Genres</option>
+          {categories.map((categorie) => (
+            <option key={categorie} value={categorie}>
+              {categorie}
+            </option>
+          ))}
+        </select>
+      </label>
     </div>
   );
 }
