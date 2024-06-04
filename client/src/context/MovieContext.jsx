37e167cf866929fc5877deb6103272a11431bece
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-06-04 13:00:16 +0200

    ajout bouton supprimer du panier avec style et logique fonctionnelle

--- client/src/context/MovieContext.jsx
+++ client/src/context/MovieContext.jsx
@@ -4,14 +4,18 @@ const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
-
+  const deleteToCart = (movie) => {
+    if (cart.some((item) => item.id === movie.id)) {
+      setCart((prev) => prev.filter((item) => item.id !== movie.id));
+    }
+  };
   const addToCart = (movie) => {
     if (!cart.some((item) => item.id === movie.id)) {
       setCart((prev) => [...prev, movie]);
     }
   };
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, deleteToCart }}>
       {children}
     </CartContext.Provider>
   );
