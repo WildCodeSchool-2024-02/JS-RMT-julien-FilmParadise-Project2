12ca1109f7caf4bd2c40699688b959088a197de1
Vanessa <nessa_g@hotmail.fr>
2024-06-05 16:24:26 +0200

    optimisation du jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <Router> {}
-      <div>
+      <>
         <Header />
         <div className="App">
           <h1>Movie List</h1>
         </div>
-      </div>
-    </Router>
+      </>
+
   );
 }
 
