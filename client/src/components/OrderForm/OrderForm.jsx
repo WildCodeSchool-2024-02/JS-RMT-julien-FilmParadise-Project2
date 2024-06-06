3afc17e7e40cbb3549bb0e63ada0bcb86b59b797
Vanessa <nessa_g@hotmail.fr>
2024-06-06 13:35:59 +0200

    Mise en place de la soumission du formulaire de commande, passer ma commande avec fenetre de confirmation

--- client/src/components/OrderForm/OrderForm.jsx
+++ client/src/components/OrderForm/OrderForm.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import { useCart } from "../../context/MovieContext";
+import "./OrderForm.css";
 
-function OrderForm() {
+function OrderForm({ totalPrice }) {
+  const {setCart} = useCart();
+  
   const [formData, setFormData] = useState({
     name: "",
     lastname: "",
     street: "",
     zipcode: "",
     country: "",
-    phone: ""
+    phone: "",
   });
 
   const [isOrderPlaced, setIsOrderPlaced] = useState(false);
@@ -16,102 +20,116 @@ function OrderForm() {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+      e.preventDefault();
     setIsOrderPlaced(true);
   };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Lastname:
-            <input
-              type="text"
-              name="lastname"
-              value={formData.lastname}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Street number & name:
-            <input
-              type="text"
-              name="street"
-              value={formData.street}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Zipcode:
-            <input
-              type="text"
-              name="zipcode"
-              value={formData.zipcode}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Country:
-            <input
-              type="text"
-              name="country"
-              value={formData.country}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Phone number:
-            <input
-              type="tel"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <button type="submit">Order</button>
-      </form>
-      {isOrderPlaced && (
-        <div className="confirmation-window">
-          <p>Thank you {formData.name} {formData.lastname} for your purchase!</p>
-          <p>You'll receive your invoice in a few days by post.</p>
-          <button type="button" onClick={() => setIsOrderPlaced(false)}>Close</button>
-        </div>
-      )}
-    </>
+  const handleClose = () => {
+  
+    setIsOrderPlaced(false);
+    setFormData({
+      name: "",
+      lastname: "",
+      street: "",
+      zipcode: "",
+      country: "",
+      phone: "",
+    });
+    setCart([]);
+  };
 
-  );
+    if (isOrderPlaced) {
+      return (
+        <div className="confirmation-window">
+        <p>Thank you {formData.name} {formData.lastname} for your purchase!</p>
+        <p>You'll receive your invoice in a few days by post.</p>
+        <p>Total: {totalPrice} €</p>
+        <button type="button" onClick={handleClose}>Close</button>
+      </div>
+    );
+  }
+    return (
+        <form className="order-form" onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Lastname:
+              <input
+                type="text"
+                name="lastname"
+                value={formData.lastname}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Street number & name:
+              <input
+                type="text"
+                name="street"
+                value={formData.street}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Zipcode:
+              <input
+                type="text"
+                name="zipcode"
+                value={formData.zipcode}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Country:
+              <input
+                type="text"
+                name="country"
+                value={formData.country}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Phone number:
+              <input
+                type="tel"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+          <button type="submit">Order</button>
+        </form>
+      ); 
 }
 
 export default OrderForm;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useCart } from "../context/MovieContext";
 
 import CartDetails from "../components/CartDetails/CartDetails";
 import Header from "../components/header/Header";
+import OrderForm from "../components/OrderForm/OrderForm";
 
 function Cart() {
   const { cart } = useCart();
@@ -14,17 +15,20 @@ function Cart() {
         {cart.length === 0 ? (
           <h2 className="empty-cart">There is nothing in your cart !</h2>
         ) : (
-          <>
-            <h2 className="title-cart">Your cart :</h2>
-            <div className="all-cart">
-              {cart.map((movie) => (
-                <CartDetails movie={movie} key={movie.title} />
+          <div className="cart-content">
+            <div className="cart-summary">
+              <h2 className="title-cart">Your cart :</h2>
+                <div className="all-cart">
+                {cart.map((movie) => (
+                 <CartDetails movie={movie} key={movie.title} />
               ))}
             </div>
             <div className="adjust-total-css">
               <h2 className="total-price box">Total : {totalPrice} €</h2>
             </div>
-          </>
+          </div>
+          <OrderForm totalPrice={totalPrice} />
+        </div>
         )}
       </div>
     </>
